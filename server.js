5f3ba4b63fe70f1617e209130d36875c3e710285
binhojulix <fabio.jdluz@gmail.com>
2020-10-28 09:31:52 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,23 @@
+const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
+
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso');
+        
+        Tabelas.init(conexao);
+        
+        const app = customExpress();
+
+        const PORT = process.env.PORT || 8080;
+
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}...`);
+          });
+
+     
+    }
+})
