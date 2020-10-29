a74434e5acbd6359dbdbdb68e4fb5a974fbafdd2
binhojulix <fabio.jdluz@gmail.com>
2020-10-29 11:08:22 -0300

    deploy

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql')
 
+
+
 const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ conexao.connect(erro => {
         
         const app = customExpress();
 
-        const PORT = process.env.PORT || 8080;
+        const PORT = process.env.PORT || 3000;
 
         app.listen(PORT, () => {
             console.log(`Servidor rodando na porta ${PORT}...`);
