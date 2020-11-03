2ebf02c33f3ce953a7d32702f66156d766b49ab0
binhojulix <fabio.jdluz@gmail.com>
2020-11-03 11:59:49 -0300

    commit

--- script-instalacao.sh
+++ script-instalacao.sh
@@ -1,8 +1,11 @@
 
 sudo apt update
 sudo apt upgrade
+
 sudo apt install git
 git --version
+
+git clone https://github.com/binhojulix/gefi.git
 
 sudo apt install nodejs
 
@@ -11,6 +14,13 @@ sudo apt install npm
 nodejs -v
 
 npm -v
+
+sudo npm install -g @angular/cli
+
+cd gefi
+
+npm install
+
 
 
 
@@ -27,3 +37,14 @@ exit;
 
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password';
 exit;
+
+
+sudo apt install build-essential
+
+sudo npm install pm2@latest -g
+
+pm2 start server.js
+
+pm2 startup systemd
+
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
